b180b64ebc29b8fa80f02a9634081669c827c674
T30G10v4 <matteo.giovagnotti@gmail.com>
2022-11-29 15:58:31 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,100 @@
+const { createApp } = Vue;
+
+createApp ({
+
+    data(){
+
+        return {
+            
+            singleTask: {
+
+                name: "",
+                isDone: false
+
+            },
+            
+            tasks: [ 
+                // {
+                //     name: "Alzarmi dal letto",
+                //     isDone: false
+                // },
+
+                // {
+                //     name: "Fare colazione",
+                //     isDone: false
+                // },
+
+                // {
+                //     name: "Accendere il computer",
+                //     isDone: false
+                // },
+
+                // {
+                //     name: "Collegarmi a Zoom",
+                //     isDone: false
+                // },
+
+                // {
+                //     name: "Ascoltare la lezione",
+                //     isDone: false
+                // },
+
+                // {
+                //     name: "Pranzare",
+                //     isDone: false
+                // },
+
+                // {
+                //     name: "Fare l'esercizio",
+                //     isDone: false
+                // },
+
+                // {
+                //     name: "Pushare la repo",
+                //     isDone: false
+                // },
+
+                // {
+                //     name: "FINITO!!!",
+                //     isDone: false
+                // },
+
+            ]
+            
+        }
+    },
+
+    created() {
+
+        axios.get("server.php").then((resp) => {
+
+            this.tasks = resp.data;
+
+        });
+
+    },
+
+    methods : {
+
+        removeTaskFromTasks : function(index) {
+
+            this.tasks.splice(index, 1);
+
+        },
+
+        changeDoneStatus : function(index) {
+
+            this.tasks[index].isDone = !this.tasks[index].isDone;
+
+        },
+
+        addNewTask: function() {
+
+            this.tasks.push({...this.singleTask});
+            this.singleTask.name = "";
+
+        } 
+
+    }
+
+}).mount("#app")
